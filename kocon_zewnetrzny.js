c13a2a28d06c41dab0e120e399dde07c704dd053
MaksKocon <125610273+MaksKocon@users.noreply.github.com>
2023-09-07 08:44:15 +0200

    Add files via upload

---
+++ kocon_zewnetrzny.js
@@ -0,0 +1,2 @@
+document.write("ostatnia modyfikacja strony".fontcolor("#C0C0C0").strike().fontsize(6)+"<br>");
+document.write(document.lastModified .fontcolor("#C0C0C0").strike().fontsize(6)+"<br>");
